f1cd6daf9c6798155a84b31a6f7bb3b166e576d4
Samuel Mardirosian <smardirosian@pedaids.org>
2017-07-24 09:21:36 -0400

    define category-vendor relationship

--- cat.py
+++ cat.py
@@ -1,7 +1,8 @@
 import os
+import csv
 
 base_path = 'C:\\Unnamed\\'
-dirs = [os.path.join(base_path, 'dir'+str(x)) for x in range(29) ][:1]
+dirs = [os.path.join(base_path, 'dir'+str(x)) for x in range(29) ]
 
 for d in dirs:
 	cats = [d for d in next(os.walk(d))[1] if d != 'ocr-pdfs']
@@ -10,17 +11,17 @@ for d in dirs:
 
 	pdf_dict = dict(map(lambda x: (x,[]), pdfs))
 
-	print(pdf_dict)
-
 	for cat in cats:
 		files = [file.rsplit('-',1)[0] for file in os.listdir(os.path.join(d,cat)) if file.endswith('.png')]
 		for f in files:
-			print(f)
 			try:
 				pdf_dict[f] += [cat]
 			except KeyError as err:
-				print(err)
+				#print(err)
 				pass;
 
 
-	print(pdf_dict)
+	with open(os.path.join(base_path, os.path.basename(os.path.normpath(d))+'-cats.csv'),'w+', newline='\n') as f:
+	    w = csv.writer(f)
+	    for pdf, categories in pdf_dict.items():
+	    	w.writerow([pdf] + categories)
